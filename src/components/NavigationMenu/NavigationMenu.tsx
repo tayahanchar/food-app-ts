8fa6d4c0950e0918341b14a4940e8d497a65f40a
Taya Hanchar <tayahanchar@gmail.com>
2025-10-21 19:28:28 +0200

    feat: add nav menu

--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -1,12 +1,25 @@
 import { Link, Outlet } from "react-router-dom";
+import styles from "./NavigationMenu.module.css";
+import Button from "../Button/Button";
 
 const NavigationMenu = () => {
   return (
-    <div>
-      <nav>
-        <Link to="/">Main</Link>
-        <Link to="/Cart">Cart</Link>
-      </nav>
+    <div className={styles.app}>
+      <div className={styles.navMenu}>
+        <p className={styles.name}>First name and last name</p>
+        <p className={styles.email}>e-mail</p>
+        <nav className={styles.nav}>
+          <Link className={styles.link} to="/">
+            <img className={styles.svg} src="./menu.svg" alt="menu" />
+            Main
+          </Link>
+          <Link className={styles.link} to="/Cart">
+            <img className={styles.svg} src="./cart.svg" alt="menu" />
+            Cart
+          </Link>
+        </nav>
+        <Button className={"button button-end"}>Log out</Button>
+      </div>
       <Outlet />
     </div>
   );
